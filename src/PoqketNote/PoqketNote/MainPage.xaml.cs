08573c70f3db68be17570908cdddd380eb41f36d
BG1supernode333 <bg.supernode@gmail.com>
2014-11-25 18:37:21 +0900

    アプリケーションバーの追加とソフトウェアキーボード非表示の実装

--- src/PoqketNote/PoqketNote/MainPage.xaml.cs
+++ src/PoqketNote/PoqketNote/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace PoqketNote
 {
@@ -24,6 +25,45 @@ namespace PoqketNote
         private void scrollview1_KeyDown(object sender, KeyEventArgs e)
         {
             scrollview1.ScrollToVerticalOffset(scrollview1.VerticalOffset + 60);
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[0]).IsEnabled = true;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[1]).IsEnabled = true;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[2]).IsEnabled = true;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[3]).IsEnabled = false;
+        }
+
+        private void TextBlock_LostFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void TextBlock_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mainTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[0]).IsEnabled = false;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[1]).IsEnabled = false;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[2]).IsEnabled = false;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[3]).IsEnabled = true;
+        }
+
+        private void mainTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[0]).IsEnabled = true;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[1]).IsEnabled = true;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[2]).IsEnabled = true;
+            ((ApplicationBarIconButton)ApplicationBar.Buttons[3]).IsEnabled = false;
+        }
+
+        private void ApplicationBarIconButton3_Click(object sender, EventArgs e)
+        {
+            this.Focus();
         }
     }
 }
